91fbcc45a291960b37112ebb0fbd7efe85a0b872
Manoj Patel <nycdays300@gmail.com>
2020-01-25 01:08:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 letters  = ["a", "b", "c", "d"]
-letters << "e"
+  element = "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
